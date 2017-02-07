8e9918a91ab5eeec0b2b4753c839d4d96bd0c99b
Harshini <harshinibariki@gmail.com>
2017-02-06 19:26:39 -0800

    Displays two cat images, name and when clicks on picture count increments

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-var clicks = 0;
 
 $(document).ready(function(){
+    var clicks = 0;
     $("#like").click(function () {
     clicks++;
     $("#counter").text(clicks);
     });
 
-
-
-
 });
+
+$(document).ready(function(){
+    var clicks = 0;
+    $("#output").click(function () {
+        clicks++;
+        $("#increment").text(clicks);
+    });
+
+})
